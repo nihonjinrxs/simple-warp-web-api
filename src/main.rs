93fe06f3806760edea79f22ecf2980226ab6c98e
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-11-09 07:07:17 +0000

    WIP add query param handling and JSON response

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use std::collections::HashMap;
 use warp::Filter;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Param(HashMap<String, String>);
+
+async fn hello(params: Param) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(warp::reply::json(&params))
+}
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello {}!", name));
+    // GET /hello?name=warp => 200 OK with body "Hello, warp!"
+    let hello = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::query::<Param>())
+        .and(warp::path::end())
+        .and_then(hello);
     
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
