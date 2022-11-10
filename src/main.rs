419de0c66123e8886e9914b2dce1beeb26c8b740
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-11-10 06:05:22 +0000

    Add some more meat to the api

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,59 @@
-use std::collections::HashMap;
 use warp::Filter;
 use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use tracing_subscriber::fmt::format::FmtSpan;
 
-#[derive(Serialize, Deserialize)]
-struct Param(HashMap<String, String>);
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Question {
+    id: String,
+    title: String,
+    content: String,
+}
 
-async fn hello(params: Param) -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(warp::reply::json(&params))
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Store(HashMap<String, Question>);
+
+impl Store {
+    fn new() -> Self {
+        Store {
+            0: Self::init()
+        }
+    }
+
+    fn init() -> HashMap<String, Question> {
+        let q = include_str!("../questions.json");
+        serde_json::from_str(q).expect("Can't read JSON")
+    }
+}
+
+async fn get_questions(param: String, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("{:?}", param);
+    Ok(warp::reply::json(&store))
 }
 
 #[tokio::main]
 async fn main() {
-    // GET /hello?name=warp => 200 OK with body "Hello, warp!"
-    let hello = warp::get()
-        .and(warp::path("hello"))
-        .and(warp::query::<Param>())
-        .and(warp::path::end())
-        .and_then(hello);
+    let log_filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "simple_warp_web_api=info,warp=info".to_owned());
+
+    let store = Store::new();
+    let store_filter = warp::any().map(move || store.clone());
+
+    tracing_subscriber::fmt()
+        // Use the filter we built above to determine which traces to record
+        .with_env_filter(log_filter)
+        // Record an event when each span closes
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
     
-    warp::serve(hello)
+    // GET /questions => 200 OK with body "Hello, warp!"
+    let get_questions = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::query::raw())
+        .and(warp::path::end())
+        .and(store_filter)
+        .and_then(get_questions);
+    
+    warp::serve(get_questions.with(warp::trace::request()))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
